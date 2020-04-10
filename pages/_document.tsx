2f4f5fd5b8e56cc9121600040f08424fde7dd1b4
Andrii Tiertyshnyi <andrii.tiertyshnyi@gmail.com>
2020-04-11 00:50:28 +0300

    Add yandex tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,6 +26,7 @@ export default class MyDocument extends Document {
             }}
           />
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="yandex-verification" content="ee6c564885fa152d" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
